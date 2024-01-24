da359f05f6b6e9f8bc7d09414e5353146262e531
Patrick Sullivan <patrick.sullivan@hey.com>
2024-01-24 13:51:46 -0600

    accept_trace

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod extractor;
 use axum::{
     body::Body,
     http::{Request, StatusCode},
+    middleware,
     routing::get,
     Router,
 };
 use std::net::SocketAddr;
+use tower::ServiceBuilder;
 use tower_http::trace::{self, TraceLayer};
 use tracing::{error, info_span, instrument, Level, Span};
 use tracing_opentelemetry::OpenTelemetrySpanExt;
@@ -23,11 +25,19 @@ async fn main() {
         .with(fmt::layer().pretty())
         .init();
 
-    let app = Router::new().route("/", get(hello_world)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(make_span)
-            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let middleware = ServiceBuilder::new()
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(make_span)
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .map_request(accept_trace);
+
+    // let middleware = TraceLayer::new_for_http()
+    //     .make_span_with(make_span)
+    //     .on_response(trace::DefaultOnResponse::new().level(Level::INFO));
+
+    let app = Router::new().route("/", get(hello_world)).layer(middleware);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
@@ -52,8 +62,14 @@ fn make_span(request: &Request<Body>) -> Span {
 pub fn accept_trace(request: Request<Body>) -> Request<Body> {
     // Current context, if no or invalid data is received.
     let parent_context = opentelemetry::global::get_text_map_propagator(|propagator| {
+        println!("PROPAGATOR: {:?}", propagator);
+        println!("HEADERS: {:?}", request.headers());
         propagator.extract(&HeaderExtractor(request.headers()))
     });
+
+    println!("PARENT CONTEXT: {:?}", parent_context);
+    println!("SPAN BEFORE: {:?}", Span::current());
     Span::current().set_parent(parent_context);
+    println!("SPAN AFTER: {:?}\n", Span::current());
     request
 }
