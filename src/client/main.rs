5943dd0fc9acb6ba6f86b3ab25b38b383e3c130f
Patrick Sullivan <patrick.sullivan@hey.com>
2024-01-24 21:43:48 -0600

    client

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,11 +1,5 @@
-use axum::{
-    body::Body,
-    http::{Request, StatusCode},
-    middleware,
-    routing::get,
-    Router,
-};
-use std::net::SocketAddr;
+use reqwest::StatusCode;
+use std::{io::BufRead, net::SocketAddr};
 use tower::ServiceBuilder;
 use tower_http::trace::{self, TraceLayer};
 use tracing::{error, info_span, instrument, Level, Span};
@@ -13,4 +7,16 @@ use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let stdin = std::io::stdin();
+    for _line in stdin.lock().lines() {
+        send().await;
+    }
+}
+
+async fn send() {
+    let client = reqwest::Client::new();
+    let url = "http://localhost:3000/";
+    let resp = client.get(url).send().await.unwrap();
+    println!("response status code: {:?}", resp.status());
+}
